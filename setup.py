67afe884135d506030b7867feb39dce55b7bb77d
EmiBrigante <ebrigs2000@outlook.com>
2025-05-10 20:16:13 +0200

    chore: Bump version to 1.0.0 for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="denexus-chatbot",
-    version="0.1.0",
+    version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
     description="A chatbot built using Retrieval Augmented Generation (RAG)",
